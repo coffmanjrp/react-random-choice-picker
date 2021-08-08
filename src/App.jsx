171af0f08c60ee7840ee7c447d359a57d575a330
coffmanjrp <coffmanjrp@gmail.com>
2021-08-07 19:06:18 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <h3>
+          Enter all of the choices divided by a comma (',') <br />
+          Press enter when you're done
+        </h3>
+        <textarea
+          className="textarea"
+          placeholder="Enter choices here..."
+        ></textarea>
+        <div>
+          <span className="tag">Choices1</span>
+          <span className="tag highlight">Choices2</span>
+          <span className="tag">Choices3</span>
+        </div>
+      </div>
     </div>
   );
 }
